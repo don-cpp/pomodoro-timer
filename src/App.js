c0de31c9173aef77734e94213d0883c9c62ee278
Daniel Oliveros Guerra <43940692+don-cpp@users.noreply.github.com>
2022-06-05 19:45:02 -0500

    Pomodoro timer first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [secondsLeft, setSecondsLeft] = useState(25 * 60);
+    const [timer, setTimer] = useState();
+
+    const start = () => {
+        const timer = setInterval(() => {
+            setSecondsLeft((secondsLeft) => secondsLeft - 1);
+            if (secondsLeft === 0) {
+                clearInterval(timer);
+            }
+        }, 1000);
+        setTimer(timer);
+    };
+
+    useEffect(() => {
+        if (secondsLeft === 0) {
+            clearInterval(timer);
+        }
+    }, [secondsLeft, timer]);
+
+    useEffect(() => {
+        return () => clearInterval(timer);
+    }, [timer]);
+
+    return (
+        <div className="App">
+            <h1>Pomodoro timer</h1>
+            <button onClick={start}>start</button>
+            <div>{secondsLeft} seconds left</div>
+        </div>
+    );
 }
-
-export default App;
